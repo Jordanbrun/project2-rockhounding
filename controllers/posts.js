1873fb56141727f5bac8598068bdcf3a2f57df18
Mallory Clark <malloryclark@Mallorys-MacBook-Pro.local>
2019-10-25 16:05:03 -0600

    edit new routes done and views with edit and new

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,76 +3,77 @@ const router = express.Router();
 const Post = require("../models/post");
 
 
-// INDEX FOR POSTS 
-router.get("/", async (req, res) => {
-	try {
-
-	} catch (err) {
+//new route
+router.get('/new', (req, res) =>{
+	if(err){
 		res.send(err);
-	}
+	} else {
+	
+	const allUsers = Users.find({});
+	const allRocks = Rocks.find({});
+	const allStates = States.find({});
+		res.render('posts/new.ejs', {
+			allUsers: Users
+			allRocks : Rocks
+			allStates : States
+		})
+	}	
+			
+})
 
-});
+// create
+router.post('/', async (req, res) =>{
+	try{
+	const foundUser = User.findById(req.body.userId);
+	const foundRock = Rock.findById(req.body.rockId);
+	const foundState = State.findById(req.body.stateId);
+	const newPost = Post.create(req.body);
+	const [foundUser, foundRock, foundState, newPost] = await
+	Promise.all([foundUser, foundRock, foundState, newPost]);
+	foundUser.posts.push(newPost);
+	foundRock.posts.push(newPost);
+	foundState.posts.push(newPost);
+	await foundUser.save();
+	await foundRock.save();
+	await foundState.save();
+	res.redirect('/posts');
 
-// NEW FOR POSTS
-router.get("/new", async (req, res) => {
-	try {
-
-	} catch(err) {
-		res.send(err);
-	}
-});
-
-// ADD A NEW POST
-
-router.post ("/", async (req, res) => {
-	try {
-
-	} catch(err) {
+	} catch {
 		res.send(err);
 	}
 
 })
 
-// SHOW FOR POSTS
 
-router.get("/:id", async (req, res) => {
-	try {
-
-	} catch(err) {
-		res.send(err);
-	}
-});
-
-// EDIT A POST
-
-router.get("/:id/edit", async (req, res) => {
-	try {
-
-	} catch(err) {
-		res.send(err);
-	}
-});
+//edit 
+router.get('/:id/edit', (req, res) =>{
+	Post.findById(req.params.id, (err, foundPost) =>{
+		if(err){
+			res.send(err);
+		} else {
+			res.render('posts/edit.ejs', {
+				post: foundPost
+				})
+			}
+		})
+	})
 
 
-// UPDATE AN EDITED POST
-
-router.put("/:id", async (req, res) => {
-	try {
-
-	} catch(err) {
-		res.send(err);
-	}
-});
-
-// DELETE A POST
-
-router.delete("/:id", async (req, res) => {
-	try {
-
-	} catch(err) {
-		res.send(err);
-	}
-});
+router.put('/:id', (req, res)=>{
+	Post.findByIdAndUpdate(req.params.id, req.body, (err, response) =>{
+		if(err){
+			res.send(err);
+		} else {
+			res.redirect('/posts');
+		}
+		
+	})
+})
 
 
-//GITHUB IS AWFUL AND I WANT TO CRY
+
+module.exports = router;
+
+// new, post, edit, put, delete
+
+//no order b/c no 
