58634085fdd1bec08db3fe38f060a0987264b3b6
Brad Donakowski <braddonako@gmail.com>
2019-10-25 10:13:25 -0600

    added the states controller outline

--- controllers/states.js
+++ controllers/states.js
@@ -3,6 +3,75 @@ const router = express.Router();
 const States = require('../models/state.js');
 
 
+//index
+router.get('/', async (req, res)=>{
+
+  try {
+  
+  
+      };
+  } catch(err){
+    res.send(err);
+  });
+
+
+// new route
+router.get('/new', async (req, res)=>{
+
+  try {
+  
+  
+      };
+  } catch(err){
+    res.send(err);
+  });
+
+
+
+//edit
+router.get('/:id/edit', async (req, res)=>{
+
+  try {
+  
+  
+      };
+  } catch(err){
+    res.send(err);
+  });
+
+router.post('/', async (req, res)=>{
+
+  try {
+  
+  
+      };
+  } catch(err){
+    res.send(err);
+  });
+
+
+router.delete('/:id', async (req, res)=>{
+
+  try {
+  
+  
+      };
+  } catch(err){
+    res.send(err);
+  });
+
+router.put('/:id', async (req, res)=>{
+
+  try {
+  
+  
+      };
+  } catch(err){
+    res.send(err);
+  });
+
+
+
 
 
 module.exports = router;
