c53dfb8b3c5a302e911645842883f89c5d43583f
Jordan Bruner <jordanbruner@Jordans-MacBook-Pro.local>
2019-10-25 09:21:33 -0600

    models

---
+++ models/rock.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const rocksSchema = mongoose.Schema ({
+	name: String,
+	body: String,
+	IMG: String,
+	users: [{
+		type: mongoose.Schema.Types.ObjectID,
+		ref: "User"
+	}],
+	states: [{
+		type: mongoose.Schema.Types.ObjectID,
+		ref: "State"
+	}],
+	posts: [{
+		type: mongoose.Schema.Types.ObjectID,
+		ref: "Post"
+
+	}]
+
+});
+
+const Rock = mongoose.model("Rock", rocksSchema);
+module.exports = Rock;
