08fd7079fc06fed76a0b593977e37b847edc5115
Brad Donakowski <braddonako@gmail.com>
2019-10-28 13:46:04 -0600

    This isnt going to work but lets try it

--- controllers/states.js
+++ controllers/states.js
@@ -24,6 +24,7 @@ router.get("/", async (req,res) => {
 router.get("/:id", async (req,res) => {
 	try {
 		const foundState = await State.findById(req.params.id);
+		console.log(req.params.id)
 		const allRocks = await Rock.find({state: foundState})
 		res.render("states/show.ejs", {
 			state: foundState,
