e2382431e7b632ac44c2a9ec35e0fb937f33ebf1
Jordan Bruner <jordanbruner@Jordans-MacBook-Pro.local>
2019-10-31 09:29:04 -0600

    updating package.json and updating code for heroku

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
+const connectionString = process.env.MONGODB_URI;
 
-const connectionString = 'mongodb://localhost/rockhounding';
 
 
 mongoose.connect(connectionString, { useNewUrlParser: true,
--- server.js
+++ server.js
@@ -4,21 +4,18 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
 require('./db/db');
-
-app.use(express.static('public'));
-
+require('dotenv').config();
 
 app.use(session({
 	secret: 'This is our secret string for passwords. A ha ha.',
 	resave: false,
 	saveUninitialized: false
-}))
+}));
+
+app.use(express.static('public'));
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: false}));
-
-
-
 
 const statesController = require('./controllers/states.js');
 app.use('/states', statesController);
@@ -32,16 +29,15 @@ app.use('/rocks', rocksController);
 const usersController = require('./controllers/users.js');
 app.use('/users', usersController);
 
-const registrationController = require('./controllers/registration.js');
-app.use('/registration', registrationController);
 
 // here is the route to our homepage
 
 app.get('/', (req, res) => {
+	req.session.test = 'test';
+	console.log(req.session);
 	res.render('index.ejs');
 });
 //seedScripting.stateSeed();
 
-app.listen(3000, () => {
-	console.log('I can hear you');
-});
+app.listen(process.env.PORT, () => {
+  console.log('listening on port 3000');
