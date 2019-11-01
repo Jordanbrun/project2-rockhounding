d732f03133fdb77add68fe9fa3301b6b82a59d42
Jordan Bruner <jordanbruner@Jordans-MacBook-Pro.local>
2019-11-01 12:07:22 -0600

    more seeds

--- seed.js
+++ seed.js
@@ -391,6 +391,91 @@ const massachusettsRockSeed = async () => {
 	}	
 }
 
+const californiaRockSeed = async () => {
+	try { 
+		const california = await State.findOne({"name": "California"});
+	    if (california.rocks !== "undefined" && california.rocks.length > 0){
+	        return;
+	    } else {
+
+		    const californiaRockArr = [
+		    {name: "Serpentinite",
+		     IMG: "serpentinite.jpg"},
+		    {name: "Benitoite", 
+			 IMG: "benitoite.jpg"}
+		    ]
+		    console.log(california.rocks);
+
+		    console.log(californiaRockArr);
+
+		    const createdRocks = await Rock.create(californiaRockArr)
+		    		for (let i=0 ; i<createdRocks.length; i++) {
+		    			console.log(createdRocks[i]);
+		    			california.rocks.push(createdRocks[i])
+		    			await california.save();
+		    			await createdRocks[i].save();
+		    		}
+
+		    		for (let i=0; i<createdRocks.length; i++) {
+		    			createdRocks[i].states.push(california);
+		    			await createdRocks[i].save();
+		    			console.log(createdRocks[i]);
+		    		}
+		}
+	} catch (err) {
+		console.log(err);
+		res.send(err);
+	}	
+}
+
+const texasRockSeed = async () => {
+	try { 
+		const texas = await State.findOne({"name": "Texas"});
+	    if (texas.rocks !== "undefined" && texas.rocks.length > 0){
+	        return;
+	    } else {
+
+		    const texasRockArr = [
+		    {name: "Cretaceous Fossils",
+		     IMG: "Cretaceous.jpg"},
+		    ]
+		    console.log(texas.rocks);
+
+		    console.log(texasRockArr);
+
+		    const createdRocks = await Rock.create(texasRockArr)
+		    		for (let i=0 ; i<createdRocks.length; i++) {
+		    			console.log(createdRocks[i]);
+		    			texas.rocks.push(createdRocks[i])
+		    			await texas.save();
+		    			await createdRocks[i].save();
+		    		}
+
+		    		for (let i=0; i<createdRocks.length; i++) {
+		    			createdRocks[i].states.push(texas);
+		    			await createdRocks[i].save();
+		    			console.log(createdRocks[i]);
+		    		}
+
+		    const topaz = await Rock.findOne({"name": "Topaz"})
+		    
+			texas.rocks.push(topaz);
+			topaz.states.push(texas)
+			await teaxs.save();
+			await topaz.save();
+
+			const agate = await Rock.findOne({"name": "Agate"})
+		    
+			texas.rocks.push(agate);
+			agate.states.push(texas);
+			await teaxs.save();
+			await agate.save();
+		}
+	} catch (err) {
+		console.log(err);
+		res.send(err);
+	}	
+}
 
 const seedAll = async () =>
 {
@@ -402,6 +487,8 @@ const seedAll = async () =>
 	await arizonaRockSeed();
 	await arkansasRockSeed();
 	await massachusettsRockSeed();
+	await californiaRockSeed();
+	await texasRockSeed();
 }
 
 seedAll();
